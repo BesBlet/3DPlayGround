4a8b3069ec11f265471ab587294b19e67cf432e4
Kemal <45937663+BesBlet@users.noreply.github.com>
2021-02-02 20:54:57 +0300

    Initial commit

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    Rigidbody rb;
+
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void ResetPosition ()
+    {
+        transform.position = new Vector3(0, 10, 0);
+        rb.velocity = Vector3.zero;
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Target"))
+        {
+            ResetPosition();
+        }
+
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            ResetPosition();
+        }
+    }
+
+
+
+}
